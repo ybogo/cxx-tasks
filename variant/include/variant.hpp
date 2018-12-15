09f16c2315693cf1c33b57d6a9c5874b4f396b28
Yuri Bogomolov <cleveryura@gmail.com>
2018-12-15 15:48:42 +0300

    variant tasks are done

--- variant/include/variant.hpp
+++ variant/include/variant.hpp
@@ -15,49 +15,60 @@
 #include <boost/variant.hpp>
 
 //{ number
-struct number
+struct number : std::variant<int, float>
 {
-
+    using variant::variant;
 };
 //}
 
 //{ array
-struct array
+struct array : std::vector<number>
 {
-
+    using vector::vector;
 };
 //}
 
 //{ recursive_array
-struct recursive_array
+struct recursive_array : std::vector<std::variant<number, std::shared_ptr<recursive_array>>>
 {
-
+    using vector::vector;
 };
 //}
 
 //{ recursive_array2
-struct recursive_array2
+struct recursive_array2 : std::vector<std::variant<number, boost::recursive_wrapper<recursive_array2>>>
 {
-
+    using vector::vector;
 };
 //}
 
 //{ variant_decorator
-template<???>
-struct variant_decorator
+template<class... Args>
+struct variant_decorator : std::variant<Args...>
 {
+	using std::variant<Args...>::variant;
 
-    ?? as()
-    {
+	template <typename T>
+    const T& as() const {
+		if constexpr (std::disjunction_v<std::is_same<T, Args>...>) {
+			return std::get<T>(*this);
+		} else {
+            return std::get<boost::recursive_wrapper<T>>(*this).get();
+		}
+	}
 
-    }
+	template <typename T>
+	T& as() {
+		return const_cast<T&>(const_cast<const variant_decorator *>(this)->as<T>());
+	}
 };
 //}
 
 //{ recursive_map
-struct recursive_map
-{
+struct recursive_map : std::map<std::string, variant_decorator<int, bool, std::string, boost::recursive_wrapper<recursive_map>>>
 
+{
+    using map::map;
 };
 //}
 
